d56cae6fac4eb625499a692e2702f6fd24d0a517
SoluChem <dlwnsgk1105@hufs.ac.kr>
2020-12-03 10:46:53 +0900

    Add scr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [val, setValue] = useState('');
+  const [state, setState] = useState([]);
+
+  function handleOnKeyPress(e) {
+    if (e.key === 'Enter') {
+      handleOnSubmit();
+    }
+  }
+
+  function handleOnSubmit() {
+    setState([val,...state]);
+    setValue('');
+  }
+
+  function handleOnChange(e) {
+    setValue(e.target.value);
+  }
+
+  function handleRemove(idx) {
+    setState(
+      [...state.slice(0,idx), ...state.slice(idx+1)]
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        value={val}
+        onChange={handleOnChange}
+        onKeyPress={handleOnKeyPress}
+      />
+      <button type='submit' onClick={handleOnSubmit}>
+        등록하기
+      </button>
+      {state.map((e, idx) => (
+        <div key={idx}>
+          <span>{e}</span>
+          &nbsp;
+          <button onClick={() => handleRemove(idx)}>제거</button> 
+        </div>
+      ))}
     </div>
   );
 }
